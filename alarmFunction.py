3e1f4cab6da05dfc49017ac19db2895056e94ce1
SaúlTejeda117 <79871748+saulTejeda117@users.noreply.github.com>
2022-09-29 15:07:16 -0500

    Add files via upload

--- alarmFunction.py
+++ alarmFunction.py
@@ -1,13 +1,34 @@
 # Alarm Clock Function
-
+#
 # GUI Dependencies
 import tkinter as tk
-
 # Events Schedule Dependencies
 # import schedule
-
+# Search file
+import os
 def main():
+	alaramWindow = tk.Tk()
+	# MainWindow Features
+	alaramWindow.title('Alarm Clock')
+	alaramWindow.configure(width=500, height=200, bg='#000000')
+	alaramWindow.resizable(False, False)
+	def show_alarm_list():
+		fileExists = os.path.exists('EventList.txt')
+		if (fileExists == True):
+			with open("EventList.txt") as f: 
+				lines = f.readlines()
+		else:
+		    with open("EventList.txt", "w") as f: f.write(hours[h]+':'+minutes[m]+':'+seconds[s]+'\n')
+		i = 0
+		# Create Screen Elements of the Alarm
+		while(len(lines)>i):
+
+			print(lines[i])
+			i+=1
+	show_alarm_list()
+	tk.Button(alaramWindow, name='add', text='Add Alarm', command=lambda:set_alarm(), width=8, font=('Helvetica',12), border = 0, bg = '#43C42A').place(x=260,y=155)
 	def set_alarm():
+		alaramWindow.destroy()
 		setAlarmWindow = tk.Tk()
 		# setAlarmWindow features
 		setAlarmWindow.title('Alarm Clock')
@@ -26,11 +47,19 @@ def main():
 		global h
 		global m
 		global s
-		def save_alarm():
+		def save_alarm(alarmName):
 			global h
 			global m
 			global s
-			print(hours[h], ':', minutes[m], ':', seconds[s])
+			fileExists = os.path.exists('EventList.txt')
+			if (fileExists == True):
+			    with open("EventList.txt", "a") as f:
+			        f.write(alarmName+' '+hours[h]+':'+minutes[m]+':'+seconds[s]+'\n')
+			else:
+			    with open("EventList.txt", "w") as f:
+			        f.write(alarmName+' '+hours[h]+':'+minutes[m]+':'+seconds[s]+'\n')
+			setAlarmWindow.destroy()
+			main()
 		def change_time_unit(time, timeUnit):
 			global h
 			global m
@@ -88,15 +117,34 @@ def main():
 		tk.Label(setAlarmWindow, text = minutes[m], name='minuteSelector', font=('Helvetica',25), fg='#43C42A', bg='#000000').place(x=225,y=22)
 		tk.Label(setAlarmWindow, text = ':', font=('Helvetica',15), fg='#43C42A', bg='#000000').place(x=265,y=27)
 		tk.Label(setAlarmWindow, text = seconds[s], name='secondsSelector', font=('Helvetica',25), fg='#43C42A', bg='#000000').place(x=275,y=22)
-		# "Repit it every" Menu
-		tk.Label(setAlarmWindow, text= 'Repit Alarm: ', font=('Helvetica',10), fg='#43C42A', bg='#000000').place(x=150,y=85)
+		# Alarm 
+		tk.Label(setAlarmWindow, text = 'Alarm Label: ', font=('Helvetica',12), fg='#43C42A', bg='#000000').place(x=150,y=90)
+		tk.Entry(setAlarmWindow, name = 'alarmEntry', width=18, bg='#43C42A').place(x=250,y=90)
+		# "Repit it every" Drop Down Menu
+		tk.Label(setAlarmWindow, text= 'Repit  Alarm: ', font=('Helvetica',12), fg='#43C42A', bg='#000000').place(x=150,y=120)
 		selectionEvent = tk.StringVar(setAlarmWindow)
 		event_repetition = ['Once','Every Day','Every Week','Every Weekend'] 
-		selectionEvent.set(event_repetition[0])
-		tk.OptionMenu(setAlarmWindow, event_repetition, *event_repetition).place(x=250,y=85)
-		# Start Button
-		tk.Button(setAlarmWindow, name='save', text='Save', command=lambda:save_alarm(), width=8, font=('Helvetica',12), border = 0, bg = '#43C42A').place(x=150,y=155)
-		tk.Button(setAlarmWindow, name='check', text='Alarms', command=lambda:stop_screen(), width=8, font=('Helvetica',12), border = 0, bg = '#43C42A').place(x=260,y=155)
-		setAlarmWindow.mainloop()
 
+		selectionEvent.set(event_repetition[0])
+		dropDownMenu = tk.OptionMenu(setAlarmWindow, selectionEvent, *event_repetition)
+		dropDownMenu.config(bg='black')
+		dropDownMenu.place(x=250,y=120)
+		
+		dropDownMenu['width']=12
+		dropDownMenu["highlightthickness"]=0
+		dropDownMenu["border"]=1
+		dropDownMenu['activebackground']='#000000'
+		dropDownMenu['activeforeground']='#43C42A'
+		dropDownMenu['bg']='#000000'
+		dropDownMenu['foreground']='#43C42A'
+		dropDownMenu['height']=1
+
+		# Get data from entryAlarm
+		alarmEntry = setAlarmWindow.nametowidget('alarmEntry')
+		# Save Alarm Button
+		tk.Button(setAlarmWindow, name='save', text='Save', command=lambda:save_alarm(alarmEntry.get()), width=8, font=('Helvetica',12), border = 0, bg = '#43C42A').place(x=150,y=160)
+		# Return to Alarm List Window
+		tk.Button(setAlarmWindow, name='check', text='Alarms', command=lambda:stop_screen(), width=8, font=('Helvetica',12), border = 0, bg = '#43C42A').place(x=260,y=160)
+		setAlarmWindow.mainloop()
+	alaramWindow.mainloop()
 main()
