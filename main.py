b8e7c36c5b2c2b50bc93b48f9a4e1c46f9a24796
SaúlTejeda117 <79871748+saulTejeda117@users.noreply.github.com>
2022-10-27 17:30:55 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,90 @@
-# Main Multifunctional Watch
+# Multifunctional Clock Features:
+# -> Stopwatch
+# -> Timer
+# -> Pomodoro
+# -> Alarm Clock
 
+from Stopwatch import main as stopwath
+from Timer import main as timer
+
+# GUI Dependencies
 import tkinter as tk
-
-
+# Clock Dependencies
+import time
 
 def main():
 	# MainWindow widgets
 	mainWindow = tk.Tk()
-	mainWindow.title('Stop Watch')
+	mainWindow.title('Multi-Functional Watch')
 	mainWindow.configure(width=500,height=200, bg='#000000')
 	mainWindow.resizable(False, False)
-	
-	def call_timer():
-		print('a')
 	def call_stopwatch():
-		print('b')
-	def call_alarm():
-		print('c')
+		mainWindow.destroy()
+		stopwath()
+	def call_timer():
+		mainWindow.destroy()
+		timer()
+	def call_alarmClock():
+		print('call_alarmClock')
 	def call_pomodoro():
-		print('d')
+		print('call_pomodoro')
 
-	# Main fuctionalities:
-	# -> Stopwatch
-	# -> Timer
-	# -> Alarm Clock
-	# -> Pomodoro
+	# Stopwatch/Timer/Pomodoro/AlarmClock Buttons 
+	tk.Button(mainWindow, 
+		text='Stopwatch',
+		fg='#43C42A', 
+		bg='#000000', 
+		font=('Helvetica', 12), 
+		width=10,
+		border=1,
+		activebackground='#43C42A',
+		command = call_stopwatch
+	).place(x=40,y=150)
+	tk.Button(mainWindow, 
+		text='Timer',
+		fg='#43C42A', 
+		bg='#000000', 
+		font=('Helvetica', 12), 
+		width=10,
+		border=1,
+		activebackground='#43C42A',
+		command = call_timer
+	).place(x=150,y=150)
+	tk.Button(mainWindow, 
+		text='Alarm Clock',
+		fg='#43C42A', 
+		bg='#000000', 
+		font=('Helvetica', 12), 
+		width=10,
+		border=1,
+		activebackground='#43C42A',
+		command = call_alarmClock
+	).place(x=260,y=150)
+	tk.Button(mainWindow, 
+		text='Pomodoro',
+		fg='#43C42A',
+		bg='#000000', 
+		font=('Helvetica', 12), 
+		width=10,
+		border=1,
+		activebackground='#43C42A',
+		command = call_pomodoro
+	).place(x=370,y=150)
 
-	tk.Button(mainWindow, name='stopwatch', text='Stopwatch', command=lambda:call_stopwatch(), width=10, font=('Helvetica',14),fg = '#43C42A', bg='#000000', border = 2).place(x=5,y=10)
-	tk.Button(mainWindow, name='timer', text='Timer', command=lambda:call_timer(), width=10, font=('Helvetica',14), border = 0, bg = '#43C42A').place(x=130,y=10)
-	tk.Button(mainWindow, name='alarm', text='Alarm Clock', command=lambda:call_alarm(), width=10, font=('Helvetica',14), border = 0, bg = '#43C42A').place(x=255,y=10)
-	tk.Button(mainWindow, name='pomodoro', text='Pomodoro', command=lambda:call_pomodoro(), width=10, font=('Helvetica',14), border = 0, bg = '#43C42A').place(x=380,y=10)
+	# Get/Print Current Time
+	def show_currentTime():
+		today = time.ctime()
+		today = today.split()
+		# Clock Label
+		tk.Label(mainWindow, 
+			text=today[3], 
+			fg='#43C42A', 
+			bg='#000000', 
+			font=('Helvetica', 80)
+		).place(x=35,y=10)
+		mainWindow.after(500, show_currentTime)
+	show_currentTime()
 	mainWindow.mainloop()
-main()
+
+if __name__ == '__main__':
+    main()
